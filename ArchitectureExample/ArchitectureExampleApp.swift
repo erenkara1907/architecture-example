bf4d8e96205a841a93e921438467f322b5b8ed27
erenkara1907 <erenkaraaa47@gmail.com>
2022-11-18 16:39:17 +0300

    Initial Commit

---
+++ ArchitectureExample/ArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureExampleApp.swift
+//  ArchitectureExample
+//
+//  Created by Eren Kara on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
